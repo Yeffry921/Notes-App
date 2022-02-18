76a8907bae552d97f60dee81e6bc934494203982
yeffry <yeffry45@gmail.com>
2022-02-18 14:30:47 -0500

    add routing routes module

---
+++ backend/routes/noteRoutes.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+
+
+// GET ALL RESOURCES
+router.get('/api/notes', (req, res) => {
+  
+})
+
+// GET A SINGLE RESOURCE
+router.get('/api/notes/:id', (req, res) => {
+  
+})
+
+// CREATE A RESOURCE
+router.post('/api/notes', (req, res) => {
+  
+})
+
+// UPDATE A RESOURCE
+router.put('/api/notes/:id', (req, res) => {
+  
+})
+
+// DELETE A RESOURCE
+router.delete('/api/notes/:id', (req, res) => {
+  
+})
+
+module.exports = router
