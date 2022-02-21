3210fc41b97e6a457160f1f16e981eee978cdd2b
yeffry <yeffry45@gmail.com>
2022-02-21 10:39:28 -0500

    connect backend to database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
-// const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3001
-
+const connectDB = require('./config/database')
 
 app.use(express.static('public'))
 app.use('/api/notes', require('./routes/noteRoutes'))
 
-
+connectDB()
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
