b521df2293ba6fe09e6316abc944cf282e9ac693
yeffry <yeffry45@gmail.com>
2022-02-21 11:16:48 -0500

    fix routing issue, add logic to get data from database

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,29 +1,41 @@
 const express = require('express')
 const router = express.Router()
-
+const Note = require('../models/noteModel')
 
 // GET ALL RESOURCES
-router.get('/api/notes', (req, res) => {
-  
+router.get('/', (req, res) => {
+  Note.find({})
+    .then((result) => {
+      res.json(result)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
 
 // GET A SINGLE RESOURCE
-router.get('/api/notes/:id', (req, res) => {
-  
+router.get('/', (req, res) => {
+  Note.findById(req.params.id)
+    .then((result) => {
+      res.json(result)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
 
 // CREATE A RESOURCE
-router.post('/api/notes', (req, res) => {
+router.post('/', (req, res) => {
   
 })
 
 // UPDATE A RESOURCE
-router.put('/api/notes/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   
 })
 
 // DELETE A RESOURCE
-router.delete('/api/notes/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   
 })
 
