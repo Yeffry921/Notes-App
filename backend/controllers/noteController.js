5c5d365da65d3e5e148914a8fcc801d6e095edf4
yeffry <yeffry45@gmail.com>
2022-02-25 20:38:08 -0500

    refactor all controller functions into async await for better readability

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,65 +1,60 @@
 const Note = require('../models/noteModel')
 
-const getAllNotes = (req, res) => {
-  Note.find({})
-  .then((result) => {
+const getAllNotes = async (req, res) => {
+  try {
+    const result = await Note.find({})
     res.status(200).json(result)
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log(error)
-  })
-}
-
-const getNote = (req, res) => {
-  Note.findById(req.params.id)
-    .then((result) => {
-      res.status(200).json(result)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-}
-
-const setNote = (req, res) => {
-  const body = req.body
-  
-  Note.create({
-    content: body.content,
-    completed: body.completed
-  })
-  .then((result) => {
-    res.status(200).json(result)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
-}
-
-const updateNote= (req, res) => {
-  const body = req.body
-
-  const changedNote = {
-    content: body.content,
-    completed: body.completed
   }
-
-  Note.findByIdAndUpdate(req.params.id, changedNote, { new: true })
-    .then((result) => {
-      res.status(200).json(result)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
 }
 
-const deleteNote = (req, res) => {
-  Note.findByIdAndDelete(req.params.id)
-    .then((result) => {
+const getNote = async (req, res) => {
+    try {
+      const result = await Note.findById(req.params.id)
       res.status(200).json(result)
-    })
-    .catch((error) => {
+    } catch (error) {
       console.log(error)
+    }
+}
+
+const setNote = async (req, res) => {
+  try {
+    const body = req.body
+  
+    const result = await Note.create({
+      content: body.content,
+      completed: body.completed
     })
+
+    res.status(200).json(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const updateNote= async (req, res) => {
+  try {
+    const body = req.body
+
+    const changedNote = {
+      content: body.content,
+      completed: body.completed
+    }
+    const result = await Note.findByIdAndUpdate(req.params.id, changedNote, { new: true })
+    res.status(200).json(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const deleteNote = async (req, res) => {
+  try {
+    const result = await Note.findByIdAndDelete(req.params.id)
+    res.status(200).json(result)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = { getAllNotes, getNote, setNote, updateNote, deleteNote }
