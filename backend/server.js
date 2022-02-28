299b94e0be15fd2e086f2834cdb81c7777720432
yeffry <yeffry45@gmail.com>
2022-02-28 16:22:45 -0500

    add error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,25 @@ app.use('/api/notes', require('./routes/noteRoutes'))
 
 connectDB()
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error)
+
+  if(error.name === 'CastError') {
+    return res.status(400).send({ error: 'Malformatted id' })
+  }
+  next(error)
+}
+
+// handler of requests with result to errors
+app.use(errorHandler)
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
